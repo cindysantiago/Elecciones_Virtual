63de6ef72d443fd99d9da7802687d7dd9beaecec
cahusari <cahusari2005@gmail.com>
2018-04-02 01:46:03 -0500

    Actualizacion partidos
    -Aplicacion de mantenibilidad al codigo de partidos.php
    -Adición de boton cancelar al formulario de edición de partidos
    -Corrección a error generado al subir archivo de imagen al guardar nuevo o al actualizar partido,  si se seleccionaba una imagen con la extensión en caracteres mayúsculas
    
    Co-Authored-By: cahusari <cahusari@users.noreply.github.com>

--- partidos.php
+++ partidos.php
@@ -3,128 +3,137 @@
 $ID=$_SESSION['ID'];
 include("verificar.php");*/
 include("include.inc.php");
-if(!isset($_REQUEST['op']))
+
+if(isset($_POST['boton2'])) //si se recibio algun valor de boton2 (Cancelar edición)
 {
-	$op="list";
-}
-else
-{
-	$op=$_REQUEST['op'];
-}
-if(isset($_REQUEST['idpartido']))
-	$idpartido=$_REQUEST['idpartido'];
-else
-	$idpartido="";
-	
-if($op=="delete" && $idpartido!="")
-{
-	$sql6="SELECT * from partidos where id_partido='$idpartido'";
-	$rs6=$db->Execute($sql6);
-	$logo=$rs6->fields['logo'];
-	$sql5="DELETE FROM partidos WHERE id_partido='$idpartido' ";
-	$rs5=$db->Execute($sql5);
-	
-	if($rs5)
-	{
-		unlink($logo);
-		$op="list";
-		$error=7;
-	}
-	else
-		$error=8;
+	$op="list"; //inicializa la variable $op con el valor list
 }
 
-if(isset($_REQUEST['boton1']))
+if(!isset($_REQUEST['op'])) // verifico si el input op no ha enviado algun valor
+{
+	$op="list"; // si no fue enviado,inicializo la variable $op con el valor list
+}
+else
+{
+	$op=$_REQUEST['op']; // si si fue enviado, paso el valor que trae a la variable $op
+}
+//
+if(isset($_REQUEST['idpartido'])) // verifico si el valor idpartido ha sido enviado
+	$idpartido=$_REQUEST['idpartido']; //si ha sido recibido lo guardo en la variable $idpartido
+else
+	$idpartido=""; //si no ha sido recibido el valor, inicializo la variable en ""
+	
+if($op=="delete" && $idpartido!="") // si la opción recibida fue delete y existe un idpartido
+{
+	$sql6="SELECT logo from partidos where id_partido='$idpartido'"; // busco el valor de logo
+	$rs6=$db->Execute($sql6);
+	$logo=$rs6->fields[0]; //asigno la ruta de la imagen del logo en la variable $logo
+	$sql5="DELETE FROM partidos WHERE id_partido='$idpartido' "; //borro el partido identificado por $idpartido
+	$rs5=$db->Execute($sql5);
+	
+	if($rs5) //si se pudo eliminar el partido
+	{
+		unlink($logo);//borro el archivo del logo del partido
+		$op="list"; //reasigno el valor de la opcion a list
+		$msg=7; //envío el mensaje correspondiente
+	}
+	else //si no se pudo eliminar el partido
+		$msg=8;//envío el mensaje correspondiente
+}
+
+if(isset($_REQUEST['boton1'])) //si se recibio algun valor de boton1
 {
 	$boton1=$_REQUEST['boton1'];
-	if($boton1=="Guardar" ) //nuevo candidato
+	if($boton1=="Guardar" ) //si el valor recibido fue Guardar
 	{	
-		$sql4="SELECT max(id_partido) FROM partidos";
+		$sql4="SELECT max(id_partido) FROM partidos"; //busco el id más alto creado
 		$rs4=$db->getOne($sql4);
-		$next=$rs4+1;
-		$nombre=$_REQUEST['nombre'];
-		$descripcion = $_REQUEST['descripcion'];
+		$next=$rs4+1; //incremento el id +1
+		$nombre=$_REQUEST['nombre']; //asigno el valor recibido en la variable $nombre
+		$descripcion = $_REQUEST['descripcion']; //asigno el valor recibido en la variable $descripcion
 		$sql5="INSERT INTO partidos (id_partido,nombre,descripcion) VALUES ('$next','$nombre','$descripcion')";
-		$rs5=$db->Execute($sql5);    
+		$rs5=$db->Execute($sql5);  // creo el nuevo partido  
 			// echo $sql1;
-		if($rs5)
+		if($rs5) //si el partido se pudo crear
 		{
 			//guardar url de la foto
 			$fichero=$_FILES['file']['name'];					
 			$tmp = explode(".", $fichero);
 			$extension = end($tmp);
-			$ruta="imagenes/perfiles/partidos/".$next.".".$extension;
-			$fichero_tipo=$extension;
+			$ruta="imagenes/perfiles/partidos/".$next.".".$extension; //creo la ruta y el nombre del archivo del logo del partido
+			$fichero_tipo=strtolower ($extension); //guardo la extension convirtiendo a minuscula los caracteres
 			$allowedExts = array("jpg", "jpeg", "gif", "png", "JPG", "GIF", "PNG");
+
 			if (( ($fichero_tipo == "jpeg")	|| ($fichero_tipo == "png") || ($fichero_tipo == "jpg")) )   //($fichero_tipo== "gif") ||
 			{
 				$fichero_tmp=$_FILES['file']['tmp_name'];	
 
-				if(copy($fichero_tmp, $ruta))
+				if(copy($fichero_tmp, $ruta))//copio el archivo del logo a la carpeta correspondiente del app
 				{
 					$sql5="UPDATE partidos SET logo='$ruta' WHERE id_partido=$next ";
-					$rs5=$db->Execute($sql5);   
+					$rs5=$db->Execute($sql5);   //agrego la ruta del archivo del logo del partido
 					if($rs5)
-						$error=1;
+						$msg=1;//envío el mensaje correspondiente
 					else
-						$error=4;
+						$msg=4;//envío el mensaje correspondiente
 				}
 				else
-					$error=4;
+					$msg=4;//envío el mensaje correspondiente
 			}
 			else
-				$error=3;
+				$msg=3;//envío el mensaje correspondiente
 		}
 		else
 		{
-				$error=2;
+				$msg=2;//envío el mensaje correspondiente
 		}	
 	}
-	else if ($boton1=="Actualizar" )
+	else if ($boton1=="Actualizar" ) //si el valor recibido fue Actualizar
 	{
-		$nombre=$_REQUEST['nombre'];
-		$descripcion = $_REQUEST['descripcion'];
+		$nombre=$_REQUEST['nombre']; //asigno el valor recibido en la variable $nombre
+		$descripcion = $_REQUEST['descripcion']; //asigno el valor recibido en la variable $descripcion
 		$sql5="UPDATE partidos SET nombre='$nombre', descripcion='$descripcion' WHERE id_partido=$idpartido ";
-		$rs5=$db->Execute($sql5);
-		if ($rs5)
+		$rs5=$db->Execute($sql5); //actualizo los campos nombre y descripcion
+		
+		if ($rs5) //si se pudo actualizar el registro
 		{
 			$fichero=$_FILES['file']['name'];
-			if ($fichero!="")
+			if ($fichero!="") //si se seleccionó algun archivo de imagen
 			{
 				//Borro el logo actual del hdd
-				$sql5="SELECT * from partidos where id_partido='$idpartido'";
+				$sql5="SELECT logo from partidos where id_partido='$idpartido'";
 				$rs5=$db->Execute($sql5);
-				$logo=$rs5->fields['logo'];
-				unlink($logo);
+				$logo=$rs5->fields[0]; //se obtiene la ruta y nombre del archivo imagen del logo del partido
+				unlink($logo); //se borra la imagen del logo del partido actual
 
 				$fichero=$_FILES['file']['name'];					
-				$tmp = explode(".", $fichero);
-				$extension = end($tmp);
-				$ruta="imagenes/perfiles/partidos/".$idpartido.".".$extension;
-				$fichero_tipo=$extension;
-				$allowedExts = array("jpg", "jpeg", "gif", "png", "JPG", "GIF", "PNG");
-				if (( ($fichero_tipo == "jpeg")	|| ($fichero_tipo == "png") || ($fichero_tipo == "jpg")) )  
+				$tmp = explode(".", $fichero); //se obtiene el nombre del archivo de imagen
+				$extension = end($tmp); //se obtiene la extension del archivo de imagen
+				$ruta="imagenes/perfiles/partidos/".$idpartido.".".$extension; //se guarda la ruta + nombre + extension
+				$fichero_tipo=strtolower ($extension); //se guarda la extension del archivo de imagen convirtiendo a minuscula los caracteres
+				//$allowedExts = array("jpg", "jpeg", "gif", "png", "JPG", "GIF", "PNG");
+				if (( ($fichero_tipo == "jpeg")	|| ($fichero_tipo == "png") || ($fichero_tipo == "jpg")) )  //se compara si es una extension permitida
 				{
 					$fichero_tmp=$_FILES['file']['tmp_name'];	
 					
 											
-					if(copy($fichero_tmp, $ruta))
+					if(copy($fichero_tmp, $ruta)) //se copia el nuevo archivo de imagen de logo del partido
 					{
-						$sql5="UPDATE partidos SET logo='$ruta' WHERE id_partido=$idpartido ";
-						$rs5=$db->Execute($sql5);   
+						$sql5="UPDATE partidos SET logo='$ruta' WHERE id_partido=$idpartido "; 
+						$rs5=$db->Execute($sql5); //se actualiza la ruta y nombre del nuevo archivo de imagen del logo del partido
 						if($rs5)
-							$error=5;
+							$msg=5; //envío el mensaje correspondiente
 						else
-							$error=4;
+							$msg=4; //envío el mensaje correspondiente
 					}
 					else
-						$error=4;
+						$msg=4; //envío el mensaje correspondiente
 				}
 				else
-					$error=3;
+					$msg=3; //envío el mensaje correspondiente
 			}
 			else
-				$error=6;
+				$msg=6; //envío el mensaje correspondiente
 		}
 	}
 }
@@ -132,14 +141,14 @@ if(isset($_REQUEST['boton1']))
 
 function menu()
 {
-	include("menu.php");
+	include("menu.php"); //se invoca al menu
 }
 
 function contenido()
 {
-	global $db,$ID,$filtro,$pagina,$error,$op,$idpartido;
+	global $db,$ID,$filtro,$pagina,$msg,$op,$idpartido;
 
-	switch ($error) 
+	switch ($msg) //se crean los casos de mensajes de resultados
 	{
  		case '1':
  			echo "<p align=center><b><font color=green>El partido político ha sido ingresado!</font></b></p>";
@@ -172,7 +181,7 @@ function contenido()
 
 	//Formulario para edición de datos
 
-	if ($op=="edit" && $idpartido!="")
+	if ($op=="edit" && $idpartido!="") //si el valor de $op es edit, mostrar formulario de edicion de partidos
 	{
 		$sql2 ="SELECT * FROM partidos WHERE id_partido='$idpartido'";
 		  	$rs2=$db->execute($sql2);
@@ -195,7 +204,10 @@ function contenido()
 		</tr>";
 			
 		echo "<tr>";
-		echo "<td>Foto:</td>
+		echo "<td>Logo del partido:</td>
+		<td align=left><img src='".$rs2->fields['logo']."' width=70px height=80px></td>
+		</tr>
+		<tr>
 		<td>
 		<input type=\"file\" id=\"file\" name=\"file\" />
 		<font color=red size=5> </font></td>
@@ -206,6 +218,7 @@ function contenido()
 		<td colspan=2><label>
 		<div align=center>
 		<input type=submit name=boton1 value=Actualizar>
+		<input type=submit name=boton2 value=Cancelar>
 		</div>
 		</label>
 		</td>
@@ -213,9 +226,9 @@ function contenido()
 		</table>
 		</form><br>";
 	}
-	if ($op=="list")
+	if ($op=="list") //si el valor de $op es list, mostrar la lista de partidos y el formulario de crear nuevo partido
 	{
-			echo "<center><h1>CREAR PARTIDO POLÍTICO</h1></center>";
+			echo "<center><h1>CREAR PARTIDO POLÍTICO</h1></center>"; // Genera el formulario crear partido
 			echo "<form name=form1 id=valform method=post action=partidos.php enctype=\"multipart/form-data\">";
 			echo "<table class=formtable align=center>";
 				
@@ -234,7 +247,7 @@ function contenido()
 			</tr>";
 				
 			echo "<tr>";
-			echo "<td>Foto:</td>
+			echo "<td>Logo del partido:</td>
 			<td>
 			<input type=\"file\" id=\"file\" name=\"file\" />
 			<font color=red size=5> * </font></td>
@@ -252,10 +265,11 @@ function contenido()
 			</table>
 			</form><br>";
 			
+			// Genera el listado de partidos en pantalla
 			echo "<center><h1>GESTIÓN DE PARTIDOS POLÍTICOS</h1></center>";
 			$sql1="SELECT id_partido,nombre,descripcion,logo FROM partidos ORDER BY nombre";
 		  	$rs1=$db->execute($sql1)->getRows();
-			if($rs1)
+			if($rs1) //si encuentra partidos creados
 			{
 				echo '<table class="table table-striped table-bordered table-hover" id="dataTable1" width="900px" align=center >';
 				echo "<thead><tr>
@@ -268,7 +282,7 @@ function contenido()
 				echo "<ul>\n";
 				echo "<tbody>";
 				$c=0;
-				foreach ($rs1 as $linea) 
+				foreach ($rs1 as $linea) //lista registro por registro de cada partido encontrado en la BD
 				{		
 					$c++;
 					$d=$c%2;
@@ -291,7 +305,7 @@ function contenido()
 				echo "</tbody>";
 				echo "</table>";
 			}
-			else
+			else //si no encuentra ningun partido creado
 				echo "<p align=center>No se encontraron resultados </p>";
 			echo "<br><br>";	 
 		}
